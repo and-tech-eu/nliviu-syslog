ccb146b366e6a0acf152bf62aecbcb341a7e04ac
Liviu <nliviu@gmail.com>
2021-03-30 13:19:14 +0300

    Add PRINTF_LIKE attribute

--- include/mgos_syslog.h
+++ include/mgos_syslog.h
@@ -1,14 +1,16 @@
 #pragma once
 
+#include "common/platform.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-void mgos_syslog_log_error(const char *app_name, const char *fmt, ...);
-void mgos_syslog_log_warn(const char *app_name, const char *fmt, ...);
-void mgos_syslog_log_info(const char *app_name, const char *fmt, ...);
-void mgos_syslog_log_notice(const char *app_name, const char *fmt, ...);
-void mgos_syslog_log_debug(const char *app_name, const char *fmt, ...);
+void mgos_syslog_log_error(const char *app_name, const char *fmt, ...) PRINTF_LIKE(2, 3);
+void mgos_syslog_log_warn(const char *app_name, const char *fmt, ...) PRINTF_LIKE(2, 3);
+void mgos_syslog_log_info(const char *app_name, const char *fmt, ...) PRINTF_LIKE(2, 3);
+void mgos_syslog_log_notice(const char *app_name, const char *fmt, ...) PRINTF_LIKE(2, 3);
+void mgos_syslog_log_debug(const char *app_name, const char *fmt, ...) PRINTF_LIKE(2, 3);
 
 #ifdef __cplusplus
 }
